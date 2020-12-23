a8c66a0222c038c51b99ffe57e4190d52fd9ebee
Emmanuel Daniels <emmanueldaniels37@gmail.com>
2020-12-22 22:32:08 -0500

    added location to drive

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,54 @@
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="map"></div>
+    <h1>{{ message }}</h1>
   </div>
+  
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+<style>
+body {
+  margin: 0;
+  padding: 0;
 }
+#map {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 800px;
+  height: 500px;
+}
+</style>
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+    };
+  },
+  mounted: function () {
+    // console.log(process.env.VUE_APP_MY_API_KEY)
+    mapboxgl.accessToken = process.env.VUE_APP_MY_API_KEY;
+    var map = new mapboxgl.Map({
+      container: "map", // container id
+      style: "mapbox://styles/mapbox/outdoors-v11", // style URL
+      center: [-86.7816, 36.1627], // starting position [lng, lat]
+      zoom: 5, // starting zoom
+    });
+  },
+  methods: {},
+  mounted: function () {
+    // Add geolocate control to the map.
+    map.addControl(
+      new mapboxgl.GeolocateControl({
+        positionOptions: {
+          enableHighAccuracy: true,
+        },
+        trackUserLocation: true,
+      })
+    );
+  },
+};
 </script>
